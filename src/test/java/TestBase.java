0e02cd431cfb2738138da060ae8ba9e1244b75e4
Elina Kulikova <elinamarsona@gmail.com>
2025-05-10 23:54:46 +0300

    first commit

---
+++ src/test/java/TestBase.java
@@ -0,0 +1,16 @@
+import io.restassured.RestAssured;
+
+import io.restassured.config.SSLConfig;
+import org.junit.jupiter.api.BeforeAll;
+
+
+public class TestBase {
+
+    @BeforeAll
+    public static void setUp() {
+        RestAssured.baseURI = "https://reqres.in";
+        RestAssured.basePath = "/api";
+        RestAssured.config = RestAssured.config()
+                .sslConfig(SSLConfig.sslConfig().relaxedHTTPSValidation()); // Игнорируем SSL
+    }
+}
